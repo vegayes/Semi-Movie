ab572ee7b715222e7b687c16efd8789399864fcf
vegayes <vegayes@naver.com>
2023-11-06 13:32:12 +0900

    eunseo semi semi

--- movieInsight/src/main/java/semi/project/movieInsight/main/MainController.java
+++ movieInsight/src/main/java/semi/project/movieInsight/main/MainController.java
@@ -1,6 +1,7 @@
 package semi.project.movieInsight.main;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -20,18 +21,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import semi.project.movieInsight.cinema.dto.Cinema;
 import semi.project.movieInsight.main.service.StaffKindService;
+import semi.project.movieInsight.member.dto.Member;
 import semi.project.movieInsight.movie.dto.Movie;
 import semi.project.movieInsight.movie.service.MovieService;
+import semi.project.movieInsight.movie.service.MovieServiceimpl;
 
 
 @Controller
+@SessionAttributes("{loginMember}")
 public class MainController {
 	
 	  @Autowired
-	    private MovieService movieService; // 영화 정보를 가져오는 서비스를 주입	
+	   private MovieService movieService; // 영화 정보를 가져오는 서비스를 주입	
+	  
+	  @Autowired
+	  private MovieServiceimpl userService; // 유저 추천순 
 	  
 	  @Autowired
 	  private StaffKindService service; // 직원친절도
@@ -47,10 +56,12 @@ public class MainController {
 
 	
 	 @GetMapping("/movie")
-	    public String getMovies(Model model) {
+	    public String getMovies(Model model, @SessionAttribute(value = "loginMember", required =false) Member loginMember ) {
 	        // 다양한 장르의 영화를 조회합니다.
-		 
 
+		 	List<Movie> userPrefMovies = new ArrayList<Movie>();
+		 	
+		 
 	        List<Movie> popularMovies = movieService.findMoviesByCategory("인기순");
 	        List<Movie> actionMovies = movieService.findMoviesByCategory("액션");
 	        List<Movie> crimeMovies = movieService.findMoviesByCategory("범죄");
@@ -59,10 +70,17 @@ public class MainController {
 	        List<Movie> comedyMovies = movieService.findMoviesByCategory("코미디");
 	        List<Movie> romanceMovies = movieService.findMoviesByCategory("로맨스");
 	        List<Movie> latestMovies = movieService.findMoviesByCategory("최신순");
-	        List<Movie> userPrefMovies = movieService.findMoviesByCategory("user 맞춤 영상");
+//	        List<Movie> userPrefMovies = movieService.findMoviesByCategory("user 맞춤 영상");
 	        List<Movie> horrorMovies = movieService.findMoviesByCategory("호러");
 	        
-
+	        if(loginMember == null) {
+	        	int memberNo = 0;
+	        	 userPrefMovies = userService.userPreMovies(memberNo);
+	        	
+	        }else {
+	        	 userPrefMovies = userService.userPreMovies(loginMember.getMemberNo());
+	        }
+	        
 	        model.addAttribute("horrorMovies", horrorMovies);
 	        model.addAttribute("popularMovies", popularMovies);
 	        model.addAttribute("actionMovies", actionMovies);
@@ -75,14 +93,16 @@ public class MainController {
 	        model.addAttribute("userPrefMovies", userPrefMovies);
 	        
 	        
+
+	        
 	        System.out.println("범죄 : " + crimeMovies);
 	
            model.addAttribute("pageType","movie");
            
             //직원친절도를 위한 영화관 이름 번호 찾기
-         List<Cinema> cinemaStaff = service.cinemaStaff();
-	
-          model.addAttribute("cinemaStaff", cinemaStaff);
+//         List<Cinema> cinemaStaff = service.cinemaStaff();
+//	
+//          model.addAttribute("cinemaStaff", cinemaStaff);
            
 
           List<Cinema> cinemaGrade = service.selectcinemaGrade();
@@ -97,7 +117,7 @@ public class MainController {
 
           // 영화관 정보 번호를 가져와서 다른 테이블에 있는 각각의 영화관 친절도 평점 조회 
            
-          System.out.println(cinemaStaff);
+//          System.out.println(cinemaStaff);
            
             //영화관 평점 더하고 평균내기 
            
